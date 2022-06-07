9e159c3f3c02ca2fb4e0914f1e59db3119aea730
Elijah Finch <elijah.finch@hotmail.com>
2022-06-07 13:52:08 -0600

    More stuff

---
+++ Tutorial/Tutorial/Animal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial
+{
+    public class Animal
+    {
+        public static int AnimalId;
+
+        public int Id;
+
+        private int NumberOfLegs;
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+          
+        private string _Name;
+        public string Sound { get; set; } = "Roar";
+
+        public Animal()
+        {
+            Id = AnimalId;
+            AnimalId++;
+        }
+
+        public Animal(int numberOfLegs, string name, string sound)
+        {
+            NumberOfLegs = numberOfLegs;
+            _Name = name;
+            Sound = sound;
+
+            Id = AnimalId;
+            AnimalId++;
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine(this.Sound);
+        }
+
+        public int GetNumberOfLegs()
+        {
+            return NumberOfLegs;
+        }
+
+        public void SetNumberOfLegs(int numberOfLegs)
+        {
+            if (numberOfLegs < 2)
+            {
+                throw new ArgumentException("Hey Animals Can't have less than 2 legs");
+            }
+
+            NumberOfLegs = numberOfLegs;
+        }
+    }
+}
---
+++ Tutorial/Tutorial/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Tutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal.AnimalId = 1;
+
+            Animal animal = new Animal();
+            animal.Name = "Bob";
+            try
+            {
+                animal.SetNumberOfLegs(4);
+                Console.WriteLine("Number Of Legs " + animal.GetNumberOfLegs());
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            animal.Sound = "Roar";
+            animal.MakeSound();
+            Console.WriteLine("Animal ID: " + animal.Id);
+
+            try
+            {
+                int.Parse("2");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally");
+            }
+
+
+            Animal animalTwo = new Animal(2, "Billy", "Meow");
+            Console.WriteLine("Number Of Legs " + animalTwo.GetNumberOfLegs());
+            Console.WriteLine("Animal ID: " + animalTwo.Id);
+        }
+    }
+}
